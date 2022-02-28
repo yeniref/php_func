0c09f5a0a16e7e7f15d9cfe543e9d4ff4c81e8db
Murat <85807376+yeniref@users.noreply.github.com>
2022-02-28 14:28:04 +0300

    Add files via upload

---
+++ fonk.php
@@ -0,0 +1,22 @@
+<?php
+function dosya_indir($url,$dosya_adi,$klasor)
+{
+ 
+$url_info = pathinfo($url);
+$uzanti = strtolower($url_info['extension']); 
+$file = ($dosya_adi) ? $dosya_adi.'.'.$uzanti : $url_info['basename'];
+$yol = $klasor."/".$file;
+ 
+$curl = curl_init($url);
+$fopen = fopen($yol,'w');
+ 
+curl_setopt($curl, CURLOPT_HEADER,0);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
+curl_setopt($curl, CURLOPT_HTTP_VERSION,CURL_HTTP_VERSION_1_0);
+curl_setopt($curl, CURLOPT_FILE, $fopen);
+ 
+curl_exec($curl);
+curl_close($curl);
+fclose($fopen);
+ 
+}
